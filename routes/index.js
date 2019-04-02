42448e60533f505f75c813815630edfc701f1938
jupegarnica <jupegarnica@gmail.com>
2019-04-02 12:45:54 +0200

    register view done

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get( '/register', function ( req, res, next ) {
 
+    res.render( 'register');
+} );
+
+router.post( '/register', function ( req, res, next ) {
+
+  console.log(req.body);
+
+
+  // res.render( 'register' , {error: 'email ya está siendo utilizado'});
+  res.render( 'register' , {message: 'Registro válido. Ya puedes hacer login'});
+
+
+
+
+} );
 
 module.exports = router;
